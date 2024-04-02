221d904414dcbd275c42295926db70626e8ede60
Roberto Maldonado <rbt.maldonado@gmail.com>
2024-04-02 16:02:49 -0500

    Reorganized code

---
+++ local_data.py
@@ -0,0 +1,8 @@
+CSV_URL_ADDRESS = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/historical_automobile_sales.csv'
+
+DROPDOWN_OPTIONS = [ {'label': 'Yearly Statistics', 'value': 'Yearly Statistics'},
+    {'label': 'Recession Period Statistics', 'value': 'Recession Period Statistics'}
+    ]
+
+# List of years 
+YEAR_LIST = [i for i in range(1980, 2024, 1)]
